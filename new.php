235d94cb0e953087ad0c0c5c5e9a17ce10c5b3ab
sugarvag <sugarvag@gmail.com>
2017-11-24 11:57:50 +0200

    Added some javascript to prevent session timeout @ program input & edit

--- new.php
+++ new.php
@@ -151,3 +151,16 @@
 </div>
 </body>
 </html>
+<script>
+	var refreshTime = 600000; // every 10 minutes in milliseconds
+	window.setInterval( function() {
+			$.ajax({
+					cache: false,
+					type: "GET",
+					url: "refresh.php",
+					success: function(data) {
+						console.log(data);
+					}
+			});
+	}, refreshTime );
+</script>
--- prog.php
+++ prog.php
@@ -226,3 +226,16 @@ $newForm->msg_updateFail = $msg_updateFail;
 </div>
 </body>
 </html>
+<script>
+	var refreshTime = 600000; // every 10 minutes in milliseconds
+	window.setInterval( function() {
+			$.ajax({
+					cache: false,
+					type: "GET",
+					url: "refresh.php",
+					success: function(data) {
+						console.log(data);
+					}
+			});
+	}, refreshTime );
+</script>
