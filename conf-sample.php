8c75ed98257c8c790b25f5bd383d92bbd47abd61
sugarvag <sugarvag@gmail.com>
2024-04-09 12:40:59 +0300

    Added config sample

--- conf-sample.php
+++ conf-sample.php
@@ -1,19 +1,21 @@
 <?php
 // Debug options
 // $prDebug: set to 1 for local testing, 0 for production
-$prDebug = 1;
+$prDebug = 0;
 // for testing when debug=1
-$prsch_name = 'ΔΙΠΕ Ηρακλείου';
-$pruid = 'dipeira';
-$prem1 = 'mail@15dim-irakl.ira.sch.gr';
+$prsch_name = '';
+$pruid = '';
+$prem1 = '';
 $prem2 = '';
 
 // DB credentials
-$prDbname = 'progs';
 $prDbhost = 'localhost';
+$prDbname = 'progs';
+$prDbusername = '';
+$prDbpassword = '';
+
 $prTable = 'progs';
 $schTable = 'progs_schools';
-$prDbusername = 'root';
-$prDbpassword = '';
+
 
 ?>
